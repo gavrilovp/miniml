52e6c94df3817b1eee7fd01e1305c1adefcf7957
Pavel Berezhnoy <pberezhnoy2005@gmail.com>
2015-06-06 20:14:07 +0300

    if-else blocks gen implemented

--- src/Emit.hs
+++ src/Emit.hs
@@ -10,7 +10,7 @@ import qualified LLVM.General.AST as AST
 import qualified LLVM.General.AST.Type as T
 import qualified LLVM.General.AST.Constant as C
 import qualified LLVM.General.AST.Float as F
-import qualified LLVM.General.AST.FloatingPointPredicate as FP
+import qualified LLVM.General.AST.IntegerPredicate as IP
 
 import Data.Word
 import Data.Int
@@ -20,6 +20,8 @@ import qualified Data.Map as Map
 
 import Codegen
 import qualified Syntax as S
+
+import Debug.Trace
 
 toType :: S.Ty -> AST.Type
 toType S.TInt = int
@@ -44,11 +46,20 @@ genFun (S.Fun name argname argtype rettype body) =
 codegenTop :: S.ToplevelCmd -> LLVM ()
 codegenTop (S.Def var_name (S.Fun name argname argtype rettype body)) = do
   define (toType rettype) name fnargs bls
-  define (toType rettype) var_name var_args var_bls
+  --define (toType rettype) var_name var_args var_bls
   where
     (fnargs, bls) = genFun (S.Fun name argname argtype rettype body)
     (var_args, var_bls) = genFun (S.Fun var_name argname argtype rettype pseudo_body)
     pseudo_body = (S.Apply (S.Var name) (S.Var argname))
+codegenTop (S.Def var_name expr) = do
+  define ty fname fnargs bls
+  globalVar ty var_name (C.Int 32 0)
+  where
+    ty' = S.TInt -- typeOf expr
+    ty = toType $ ty'
+    fname = var_name ++ "_fn"
+    (fnargs, bls) = genFun (S.Fun fname "_" ty' ty' expr)
+
 codegenTop (S.Expr exp) = do
   define int "main" [] blks
   where
@@ -73,10 +84,39 @@ cgen (S.Var x) = getvar x >>= load
 cgen (S.Int n) = return $ cons $ C.Int 32 n
 cgen (S.Bool True) = return $ cons $ C.Int 1 1
 cgen (S.Bool False) = return $ cons $ C.Int 1 0
-cgen (S.Apply fn arg) = do
+cgen (S.Apply (S.Var fn) arg) = do
   larg <- cgen arg
-  lfn <- cgen fn
-  call lfn [larg]
+  call (externf int (AST.Name fn)) [larg]
+cgen (S.If cond tr fl) = do
+  ifthen <- addBlock "if.then"
+  ifelse <- addBlock "if.else"
+  ifexit <- addBlock "if.exit"
+
+  -- %entry
+  ------------------
+  cond <- cgen cond
+  test <- f_cmp IP.NE (false) cond
+  cbr test ifthen ifelse -- Branch based on the condition
+
+  -- if.then
+  ------------------
+  setBlock ifthen
+  trval <- cgen tr       -- Generate code for the true branch
+  br ifexit              -- Branch to the merge block
+  ifthen <- getBlock
+
+  -- if.else
+  ------------------
+  setBlock ifelse
+  flval <- cgen fl       -- Generate code for the false branch
+  br ifexit              -- Branch to the merge block
+  ifelse <- getBlock
+
+  -- if.exit
+  ------------------
+  setBlock ifexit
+  phi int [(trval, ifthen), (flval, ifelse)]
+
 cgen binary =
   let (a, b, fn) = _fn binary
   in do
